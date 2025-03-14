9fbea8e8818f56f4690d415d55c81e932b66eda4
wang2135 <wang2135@e.ntu.edu.sg>
2025-03-14 11:52:59 +0000

    1

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ def main():
     user_name = request.form.get("q")
     return(render_template("main.html"))
 
+@app.route("/foodexp_pred", methods=["POST", "GET"])
+def foodexp_pred():
+    q = float(request.form.get("q"))
+    return (render_template("foodexp_pred.html", r=(q*0.4851)+147.4))
+
+@app.route("/foodexp", methods = ["POST", "GET"])
+def foodexp():
+    return(render_template("foodexp.html"))
+
 @app.route("/ethical_test", methods = ["POST", "GET"])
 def ethical_test():
     return(render_template("ethical_test.html"))
@@ -22,6 +31,6 @@ def test_result():
         return render_template("pass.html")
     elif answer == "true":
         return render_template("fail.html")
-    
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
